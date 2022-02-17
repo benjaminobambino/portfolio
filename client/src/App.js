2e04e99ede703d0efb28d455941354ace0d3e39c
BenjaminoBambino <benjaminlpeck@gmail.com>
2022-02-17 11:34:32 -0800

    created and added each component to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import Intro from './components/Intro';
+import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Resume from './components/Resume';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <Intro />
+      <About />
+      <Skills />
+      <Projects />
+      <Resume />
+      <Contact />
     </div>
   );
 }
