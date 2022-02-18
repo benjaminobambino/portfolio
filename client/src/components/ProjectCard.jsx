d6c329537e6381b975cc9c62217eb33fd2227592
BenjaminoBambino <benjaminlpeck@gmail.com>
2022-02-17 21:25:55 -0800

    started projects

--- client/src/components/ProjectCard.jsx
+++ client/src/components/ProjectCard.jsx
@@ -4,8 +4,12 @@ const ProjectCard = ({ project }) => {
   return (
     <div className="project-card">
       <h3>{project.name}</h3>
-      <img src={project.pic} alt={project.name} />
+      <img src={project.pic} alt={project.name} className="project-image" />
       <p>{project.description}</p>
+      <section className="button-container">
+        <a href={project.deployed} target="_blank" rel="noopener noreferrer"><button>Deployed App</button></a>
+        <a href={project.gitHub} target="_blank" rel="noopener noreferrer"><button>GitHub</button></a>
+      </section>
     </div>
   )
 }
--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import ProjectCard from './ProjectCard'
+import choreChart from '../project-pics/chore-chart.png'
+import creatureCollab from '../project-pics/creature-collab.png'
+import goodDadBadJokes from '../project-pics/good-dad-bad-jokes.png'
+import videoGameDb from '../project-pics/video-game-database.png'
+import war from '../project-pics/war.png'
+import ticTacToe from '../project-pics/tic-tac-toe.png'
 
 const Projects = () => {
+  const projects = [
+    {
+      name: 'Chore Chart',
+      pic: choreChart,
+      description: '',
+      deployed: 'https://chorechart.netlify.app/',
+      gitHub: 'https://github.com/benjaminobambino/chore-chart'
+    },
+    {
+      name: 'Creature Collab',
+      pic: creatureCollab,
+      description: '',
+      deployed: 'https://the-creature-collab.herokuapp.com/',
+      gitHub: 'https://github.com/moomoomewmew/the-creature-collab'
+    },
+  ]
   return (
-    <div>
+    <div className="projects">
       <h1>Projects</h1>
+      <div className="projects-container">
+        { projects.map((project) => {
+          return(
+            <ProjectCard key={project.name} project={project} />
+          )
+        })}
+      </div>
     </div>
   )
 }
