84a3e4787fb2ea541edec842c71382ad7844a6e3
BenjaminoBambino <benjaminlpeck@gmail.com>
2022-02-21 00:22:47 -0800

    removed semicolons from intro

--- client/src/components/Intro.jsx
+++ client/src/components/Intro.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Intro = () => {
   return (
     <div className="intro">
-        <p className="typed-out type1">const name = 'Benjamin Peck';</p>
-        <p className="typed-out type2">const title = 'Full-Stack Software Engineer';</p>
+        <p className="typed-out type1">const name = 'Benjamin Peck'</p>
+        <p className="typed-out type2">const title = 'Full-Stack Software Engineer'</p>
         <p className="typed-out type3">const readOn = ( ) =&gt; &#123;</p>
         <p className="typed-out type4"><span className="indent">scroll( )</span></p>
-        <p className="typed-out type5">&#125;;</p>
+        <p className="typed-out type5">&#125;</p>
     </div>
   )
 }
