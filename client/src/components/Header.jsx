a4151df95986bc8b3f9c22f72acf7d0b9c1deea0
BenjaminoBambino <benjaminlpeck@gmail.com>
2022-02-20 23:32:10 -0800

    updated colors

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-scroll'
-import logo from '../blp-logo.png'
+import logo from '../blp-logo-bw.png'
 
 const Header = () => {
   return (
-    <nav>
-      <ul className="header">
-      <Link
+    <div className="header-container">
+      <nav id="main-menu">
+        <ul className="header">
+          <Link
             activeClass="active"
             to="intro"
             spy={true}
@@ -14,74 +15,157 @@ const Header = () => {
             duration={500}>
             <img src={logo} alt="logo" className="logo"/>
           </Link>
-        <li>
-          <Link
-            activeClass="active"
-            to="intro"
-            spy={true}
-            smooth={true}
-            offset={-100}
-            duration={500}>
-            Welcome
-          </Link>
-        </li>
-        <li>
-          <Link
-            activeClass="active"
-            to="about"
-            spy={true}
-            smooth={true}
-            offset={-90}
-            duration={500}>
-            About
-          </Link>
-        </li>
-        <li>
-          <Link
-            activeClass="active"
-            to="skills"
-            spy={true}
-            smooth={true}
-            offset={-90}
-            duration={500}>
-            Repertoire
-          </Link>
-        </li>
-        <li>
-          <Link
-            activeClass="active"
-            to="projects"
-            spy={true}
-            smooth={true}
-            offset={-90}
-            duration={500}>
-            Projects
-          </Link>
-        </li>
-        <li>
-          <Link
-            activeClass="active"
-            to="contact"
-            spy={true}
-            smooth={true}
-            offset={-90}
-            duration={500}>
-            Connect
-          </Link>
-        </li>
-        <li>
-          <Link
-            activeClass="active"
-            to="resume"
-            spy={true}
-            smooth={true}
-            offset={-70}
-            duration={500}>
-            Resume
-          </Link>
-        </li>
-      </ul>
-    </nav>
+          <li>
+            <Link
+              activeClass="active"
+              to="intro"
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={500}>
+              Welcome
+            </Link>
+          </li>
+          <li>
+            <Link
+              activeClass="active"
+              to="about"
+              spy={true}
+              smooth={true}
+              offset={-80}
+              duration={500}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              activeClass="active"
+              to="skills"
+              spy={true}
+              smooth={true}
+              offset={-80}
+              duration={500}>
+              Repertoire
+            </Link>
+          </li>
+          <li>
+            <Link
+              activeClass="active"
+              to="projects"
+              spy={true}
+              smooth={true}
+              offset={-80}
+              duration={500}>
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link
+              activeClass="active"
+              to="contact"
+              spy={true}
+              smooth={true}
+              offset={-80}
+              duration={500}>
+              Connect
+            </Link>
+          </li>
+          <li>
+            <Link
+              activeClass="active"
+              to="resume"
+              spy={true}
+              smooth={true}
+              offset={-80}
+              duration={500}>
+              Resume
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      <button id="hamburger-menu">
+        <nav id="sidebar-menu">
+          <ul>
+            <Link
+              activeClass="active"
+              to="intro"
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={500}>
+              <img src={logo} alt="logo" className="logo"/>
+            </Link>
+            <li>
+              <Link
+                activeClass="active"
+                to="intro"
+                spy={true}
+                smooth={true}
+                offset={-100}
+                duration={500}>
+                Welcome
+              </Link>
+            </li>
+            <li>
+              <Link
+                activeClass="active"
+                to="about"
+                spy={true}
+                smooth={true}
+                offset={-90}
+                duration={500}>
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                activeClass="active"
+                to="skills"
+                spy={true}
+                smooth={true}
+                offset={-90}
+                duration={500}>
+                Repertoire
+              </Link>
+            </li>
+            <li>
+              <Link
+                activeClass="active"
+                to="projects"
+                spy={true}
+                smooth={true}
+                offset={-90}
+                duration={500}>
+                Projects
+              </Link>
+            </li>
+            <li>
+              <Link
+                activeClass="active"
+                to="contact"
+                spy={true}
+                smooth={true}
+                offset={-90}
+                duration={500}>
+                Connect
+              </Link>
+            </li>
+            <li>
+              <Link
+                activeClass="active"
+                to="resume"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}>
+                Resume
+              </Link>
+            </li>
+          </ul>
+        </nav>
+    </button>
+  </div>
   );
 };
 
--- client/src/components/Intro.jsx
+++ client/src/components/Intro.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Intro = () => {
   return (
     <div className="intro">
-      <p>const name = 'Benjamin Peck';</p>
-      <p>const title = 'Full-Stack Software Engineer';</p>
-      <p>const readOn = () =&gt; &#123;</p>
-      <p><span className="indent">scroll()</span></p>
-      <p>&#125;;</p>
+        <p className="typed-out type1">const name = 'Benjamin Peck';</p>
+        <p className="typed-out type2">const title = 'Full-Stack Software Engineer';</p>
+        <p className="typed-out type3">const readOn = () =&gt; &#123;</p>
+        <p className="typed-out type4"><span className="indent">scroll()</span></p>
+        <p className="typed-out type5">&#125;;</p>
     </div>
   )
 }
