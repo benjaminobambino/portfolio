d3c2c9df5a436912bf31fe8e0432fe9bdd82cfae
BenjaminoBambino <benjaminlpeck@gmail.com>
2022-03-09 10:24:44 -0800

    updated resume PNGs

--- client/src/components/Resume.jsx
+++ client/src/components/Resume.jsx
@@ -1,7 +1,4 @@
 import React from 'react'
-import resume from '../Benjamin-Peck-Resume.pdf'
-import resumeOne from '../Benjamin-Peck-Resume-page-1.png'
-import resumeTwo from '../Benjamin-Peck-Resume-page-2.png'
 
 const Scroll = require('react-scroll');
 const scroll = Scroll.animateScroll;
@@ -11,10 +8,10 @@ const Resume = () => {
     <div className="resume">
       <h1>My Resume</h1>
       <section className="resume-container">
-        <img src={resumeOne} alt="resume" className="resume-image" />
-        <img src={resumeTwo} alt="resume" className="resume-image" />
+        <img src="/Benjamin-Peck-Resume-page-1.png" alt="resume" className="resume-image" />
+        <img src="/Benjamin-Peck-Resume-page-2.png" alt="resume" className="resume-image" />
       </section>
-      <a href={resume} download="Benjamin-L-Peck-Resume"><button>Download My Resume</button></a>
+      <a href="/Benjamin-Peck-Resume.pdf" download="Benjamin-L-Peck-Resume"><button>Download My Resume</button></a>
       <h4 onClick={() => scroll.scrollToTop()}>Back to Top</h4>
     </div>
   )
