2f2f2fd8ec5fc70775809e590eb2e1e5003ae8b0
Benjamin Peck <benjaminlpeck@gmail.com>
2023-05-18 15:28:51 -0700

    fix Java constants

--- client/src/components/intro-sub-components/IntroJava.jsx
+++ client/src/components/intro-sub-components/IntroJava.jsx
@@ -4,8 +4,8 @@ const IntroJava = () => {
 
     return (
       <div className="intro">
-          <p className="typed-out type1">private static final String name = "Benjamin Peck";</p>
-          <p className="typed-out type2">private static final String title = "Full-Stack Software Engineer";</p>
+          <p className="typed-out type1">private static final String NAME = "Benjamin Peck";</p>
+          <p className="typed-out type2">private static final String TITLE = "Full-Stack Software Engineer";</p>
           <p className="typed-out type3">public static void readOn ( )  &#123;</p>
           <p className="typed-out type4"><span className="indent">scroll( );</span></p>
           <p className="typed-out type5">&#125;</p>
