7717e20104c8eecd51834d0b1440af9f26c6a236
Benjamin Peck <benjaminlpeck@gmail.com>
2023-05-18 15:27:44 -0700

    add python intro

--- client/src/components/intro-sub-components/CurrentIntro.jsx
+++ client/src/components/intro-sub-components/CurrentIntro.jsx
@@ -2,21 +2,19 @@ import React from 'react'
 import IntroJS from './IntroJS'
 import IntroTS from './IntroTS'
 import IntroJava from './IntroJava'
+import IntroPython from './IntroPython'
 
 const CurrentIntro = ({currentLang}) => {
 
-  if (currentLang === "JavaScript") {
-    return (
-      <IntroJS />
-    )
-  } else if (currentLang === "TypeScript") {
-    return (
-      <IntroTS />
-    )     
-  } else if (currentLang === "Java") {
-    return (
-      <IntroJava />
-    )     
+  switch(currentLang) {
+    case "TypeScript":
+      return (<IntroTS />)
+    case "Java":
+      return (<IntroJava />)
+    case "Python":
+      return (<IntroPython />)
+    default:
+      return (<IntroJS />)
   }
 }
 
