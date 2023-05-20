b09ddfd8f8634b1d367f7533cea604c2abcf4a2e
Benjamin Peck <benjaminlpeck@gmail.com>
2023-05-20 11:45:42 -0700

    add java mobile intro

--- client/src/components/intro-sub-components/CurrentIntro.jsx
+++ client/src/components/intro-sub-components/CurrentIntro.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import IntroJS from './IntroJS'
 import IntroTS from './IntroTS'
 import IntroJava from './IntroJava'
 import IntroPython from './IntroPython'
 import IntroSql from './IntroSQL'
+import IntroJavaMobile from './mobile-intro/IntroJavaMobile'
 
 const CurrentIntro = ({currentLang}) => {
+  const [width, setWidth] = useState(1000);
+
+  useEffect(() => {
+    setWidth(window.innerWidth)
+  }, [])
 
   switch(currentLang) {
     case "TypeScript":
       return (<IntroTS />)
     case "Java":
-      return (<IntroJava />)
+      return (width > 500 ? <IntroJava /> : <IntroJavaMobile />)
     case "Python":
       return (<IntroPython />)
     case "SQL":
---
+++ client/src/components/intro-sub-components/mobile-intro/IntroJavaMobile.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const IntroJavaMobile = () => {
+
+    return (
+      <div className="intro">
+          <p className="typed-out java-type1">private static final String NAME =</p>
+          <p className="typed-out java-type2"><span className="indent">"Benjamin Peck";</span></p>
+          <p className="typed-out java-type3">private static final String TITLE =</p>
+          <p className="typed-out java-type4"><span className="indent">"Full-Stack Software Engineer";</span></p>
+          <p className="typed-out java-type5">public static void readOn()  &#123;</p>
+          <p className="typed-out java-type6"><span className="indent">scroll();</span></p>
+          <p className="typed-out java-type7">&#125;</p>
+      </div>
+    )     
+}
+
+export default IntroJavaMobile
