599d80d2677ecd965f4cdf2cf6e0b165278500bc
Benjamin Peck <benjaminlpeck@gmail.com>
2023-05-20 17:59:43 -0700

    style ts mobile intro

--- client/src/components/intro-sub-components/mobile-intro/IntroTSMobile.jsx
+++ client/src/components/intro-sub-components/mobile-intro/IntroTSMobile.jsx
@@ -4,11 +4,12 @@ const IntroTSMobile = () => {
 
     return (
       <div className="intro">
-          <p className="typed-out type1">const name: string = 'Benjamin Peck'</p>
-          <p className="typed-out type2">const title: string = 'Full-Stack Software Engineer'</p>
-          <p className="typed-out type3">const readOn = (): void =&gt; &#123;</p>
-          <p className="typed-out type4"><span className="indent">scroll()</span></p>
-          <p className="typed-out type5">&#125;</p>
+          <p className="typed-out ts-type1">const name: string = 'Benjamin Peck'</p>
+          <p className="typed-out ts-type2">const title: string =</p>
+          <p className="typed-out ts-type3"><span className="indent">'Full-Stack Software Engineer'</span></p>
+          <p className="typed-out ts-type4">const readOn = (): void =&gt; &#123;</p>
+          <p className="typed-out ts-type5"><span className="indent">scroll()</span></p>
+          <p className="typed-out ts-type6">&#125;</p>
       </div>
     )     
 }
